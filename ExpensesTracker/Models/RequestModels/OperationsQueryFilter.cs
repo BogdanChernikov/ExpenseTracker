d5446faf6244829c02df3ff5f247df71c69ade90
BogdanChernikov <bogdan.chernikov15@gmail.com>
2021-08-10 16:54:09 +0300

    GetFilteredOperations moved to Storage.

---
+++ ExpensesTracker/Models/RequestModels/OperationsQueryFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExpensesTracker.Models.RequestModels
+{
+    public class OperationsQueryFilter
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string SearchText { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
